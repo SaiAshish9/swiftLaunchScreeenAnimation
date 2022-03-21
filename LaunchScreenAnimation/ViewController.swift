1a80ea5d2e8016ab0d1bacb89431b61522639479
saiashish9 <saiashish3760@gmail.com>
2022-03-21 14:09:38 +0530

    initial commit

---
+++ LaunchScreenAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LaunchScreenAnimation
+//
+//  Created by Sai Ashish Darapureddy on 21/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
