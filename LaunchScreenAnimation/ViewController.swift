9d2d8c76dca15edf3929f7e205683e1abb576834
saiashish9 <saiashish3760@gmail.com>
2022-03-21 15:07:49 +0530

    completed

--- LaunchScreenAnimation/ViewController.swift
+++ LaunchScreenAnimation/ViewController.swift
@@ -3,7 +3,7 @@ import UIKit
 class ViewController: UIViewController {
 
     private let imageView: UIImageView = {
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 338, height: 601))
         imageView.image = UIImage(named: "logo")
         return imageView
     }()
@@ -11,13 +11,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(imageView)
-        view.backgroundColor = .white
+//        view.backgroundColor = .white
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         imageView.center = view.center
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+            self.animate()
+        })
     }
-
+    
+    private func animate(){
+        UIView.animate(withDuration: 1, animations: {
+            let size = self.view.frame.size.width * 3
+            let diffX = size - self.view.frame.size.width
+            let diffY = self.view.frame.height - size
+            self.imageView.frame = CGRect(x: -(diffX/2), y: diffY/2, width: size, height: size)
+        })
+        UIView.animate(withDuration: 1.5, animations: {
+            self.imageView.alpha = 0
+        },completion: {
+            done in if done {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                    let viewController = HomeViewController()
+                    viewController.modalTransitionStyle = .crossDissolve
+                    viewController.modalPresentationStyle = .fullScreen
+                    self.present(viewController,animated: true)
+                })
+            }
+        })
+    
+    }
+    
 }
 
